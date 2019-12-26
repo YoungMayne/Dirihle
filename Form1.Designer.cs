fbdb4c2a20c27cc3714e81fbf73f6429f87cba76
Ilya <ostrovsky1699@gmail.com>
2019-12-27 01:09:38 +0300

    Created project

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Dirihle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Table = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Nbox = new System.Windows.Forms.NumericUpDown();
+            this.Mbox = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NmaxBox = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AccuracyBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.YoBox = new System.Windows.Forms.NumericUpDown();
+            this.YnBox = new System.Windows.Forms.NumericUpDown();
+            this.XoBox = new System.Windows.Forms.NumericUpDown();
+            this.XnBox = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dsafgasdg = new System.Windows.Forms.Label();
+            this.IterLabel = new System.Windows.Forms.Label();
+            this.AccMaxLabel = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.MaxDifLabel = new System.Windows.Forms.Label();
+            this.DotLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mbox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NmaxBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YoBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YnBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XoBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XnBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Table
+            // 
+            this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table.Location = new System.Drawing.Point(307, 106);
+            this.Table.Name = "Table";
+            this.Table.Size = new System.Drawing.Size(481, 332);
+            this.Table.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 299);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Число разбиений по х:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 325);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Число разбиений по у:";
+            // 
+            // Nbox
+            // 
+            this.Nbox.Location = new System.Drawing.Point(140, 297);
+            this.Nbox.Name = "Nbox";
+            this.Nbox.Size = new System.Drawing.Size(120, 20);
+            this.Nbox.TabIndex = 3;
+            // 
+            // Mbox
+            // 
+            this.Mbox.Location = new System.Drawing.Point(140, 323);
+            this.Mbox.Name = "Mbox";
+            this.Mbox.Size = new System.Drawing.Size(120, 20);
+            this.Mbox.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 52);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Решить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 273);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Макс. число шагов:";
+            // 
+            // NmaxBox
+            // 
+            this.NmaxBox.Location = new System.Drawing.Point(140, 271);
+            this.NmaxBox.Name = "NmaxBox";
+            this.NmaxBox.Size = new System.Drawing.Size(120, 20);
+            this.NmaxBox.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 352);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Точность метода:";
+            // 
+            // AccuracyBox
+            // 
+            this.AccuracyBox.Location = new System.Drawing.Point(140, 349);
+            this.AccuracyBox.Name = "AccuracyBox";
+            this.AccuracyBox.Size = new System.Drawing.Size(120, 20);
+            this.AccuracyBox.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Границы на оси х:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Границы на оси у:";
+            // 
+            // YoBox
+            // 
+            this.YoBox.Location = new System.Drawing.Point(140, 224);
+            this.YoBox.Name = "YoBox";
+            this.YoBox.Size = new System.Drawing.Size(59, 20);
+            this.YoBox.TabIndex = 12;
+            // 
+            // YnBox
+            // 
+            this.YnBox.Location = new System.Drawing.Point(201, 224);
+            this.YnBox.Name = "YnBox";
+            this.YnBox.Size = new System.Drawing.Size(59, 20);
+            this.YnBox.TabIndex = 13;
+            // 
+            // XoBox
+            // 
+            this.XoBox.Location = new System.Drawing.Point(140, 198);
+            this.XoBox.Name = "XoBox";
+            this.XoBox.Size = new System.Drawing.Size(59, 20);
+            this.XoBox.TabIndex = 14;
+            // 
+            // XnBox
+            // 
+            this.XnBox.Location = new System.Drawing.Point(201, 198);
+            this.XnBox.Name = "XnBox";
+            this.XnBox.Size = new System.Drawing.Size(59, 20);
+            this.XnBox.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(304, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(116, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Проведено итераций:";
+            // 
+            // dsafgasdg
+            // 
+            this.dsafgasdg.AutoSize = true;
+            this.dsafgasdg.Location = new System.Drawing.Point(304, 33);
+            this.dsafgasdg.Name = "dsafgasdg";
+            this.dsafgasdg.Size = new System.Drawing.Size(123, 13);
+            this.dsafgasdg.TabIndex = 17;
+            this.dsafgasdg.Text = "Достигнутая точность:";
+            // 
+            // IterLabel
+            // 
+            this.IterLabel.AutoSize = true;
+            this.IterLabel.Location = new System.Drawing.Point(433, 9);
+            this.IterLabel.Name = "IterLabel";
+            this.IterLabel.Size = new System.Drawing.Size(13, 13);
+            this.IterLabel.TabIndex = 18;
+            this.IterLabel.Text = "0";
+            // 
+            // AccMaxLabel
+            // 
+            this.AccMaxLabel.AutoSize = true;
+            this.AccMaxLabel.Location = new System.Drawing.Point(433, 33);
+            this.AccMaxLabel.Name = "AccMaxLabel";
+            this.AccMaxLabel.Size = new System.Drawing.Size(13, 13);
+            this.AccMaxLabel.TabIndex = 19;
+            this.AccMaxLabel.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(304, 60);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(228, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Максимальная разница двух приближений:";
+            // 
+            // MaxDifLabel
+            // 
+            this.MaxDifLabel.AutoSize = true;
+            this.MaxDifLabel.Location = new System.Drawing.Point(538, 60);
+            this.MaxDifLabel.Name = "MaxDifLabel";
+            this.MaxDifLabel.Size = new System.Drawing.Size(13, 13);
+            this.MaxDifLabel.TabIndex = 21;
+            this.MaxDifLabel.Text = "0";
+            // 
+            // DotLabel
+            // 
+            this.DotLabel.AutoSize = true;
+            this.DotLabel.Location = new System.Drawing.Point(304, 82);
+            this.DotLabel.Name = "DotLabel";
+            this.DotLabel.Size = new System.Drawing.Size(45, 13);
+            this.DotLabel.TabIndex = 22;
+            this.DotLabel.Text = "В точке";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Dirihle.Properties.Resources.Picture;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(222, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.DotLabel);
+            this.Controls.Add(this.MaxDifLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.AccMaxLabel);
+            this.Controls.Add(this.IterLabel);
+            this.Controls.Add(this.dsafgasdg);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.XnBox);
+            this.Controls.Add(this.XoBox);
+            this.Controls.Add(this.YnBox);
+            this.Controls.Add(this.YoBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AccuracyBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.NmaxBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Mbox);
+            this.Controls.Add(this.Nbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Nbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mbox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NmaxBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YoBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YnBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XoBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XnBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView Table;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown Nbox;
+        private System.Windows.Forms.NumericUpDown Mbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown NmaxBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AccuracyBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown YoBox;
+        private System.Windows.Forms.NumericUpDown YnBox;
+        private System.Windows.Forms.NumericUpDown XoBox;
+        private System.Windows.Forms.NumericUpDown XnBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label dsafgasdg;
+        private System.Windows.Forms.Label IterLabel;
+        private System.Windows.Forms.Label AccMaxLabel;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label MaxDifLabel;
+        private System.Windows.Forms.Label DotLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dirihle
+{
+    public partial class Form1 : Form
+    {
+        private double h;
+        private double h2;
+        private double k;
+        private double k2;
+        private double a2;
+        private double Xn = 1;
+        private double Xo = -1;
+        private double Yn = 1;
+        private double Yo = -1;
+        private double acc_max = 0.0000001;
+        private double accuracy = 0.0;
+
+        private double[,] v;
+
+        uint counter = 0u;
+        private uint Nmax;
+        private uint N;
+        private uint M;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            N = 1;
+            M = 1;
+
+            XoBox.Minimum = decimal.MinValue;
+            XoBox.Maximum = decimal.MaxValue;
+            XnBox.Minimum = decimal.MinValue;
+            XnBox.Maximum = decimal.MaxValue;
+
+            YoBox.Minimum = decimal.MinValue;
+            YoBox.Maximum = decimal.MaxValue;
+            YnBox.Minimum = decimal.MinValue;
+            YnBox.Maximum = decimal.MaxValue;
+
+            XoBox.Value = -1;
+            XnBox.Value = +1;
+            YoBox.Value = -1;
+            YnBox.Value = +1;
+
+            AccuracyBox.Text = "0.00001";
+            NmaxBox.Value = 100;
+            Nbox.Value = 3;
+            Mbox.Value = 3;
+
+            Table.RowHeadersVisible = false;
+            Table.ColumnHeadersVisible = false;
+        }
+
+
+        private void ZeidelMethod()
+        {
+            v = new double[N + 1, M + 1];
+
+            h = (Xn - Xo) / N;
+            h2 = -Math.Pow(N / (Xn - Xo), 2);
+            k = (Yn - Yo) / M;
+            k2 = -Math.Pow(M / (Yn - Yo), 2);
+            a2 = -2.0 * (h2 + k2);
+
+            for (uint i = 0u; i < N + 1; ++i)
+            {
+                v[i, 0] = mu3(X(i));
+
+                for (uint j = 1u; j < M + 1; ++j)
+                {
+                    v[i, j] = 0.0;
+                }
+
+                v[i, M] = mu4(X(i));
+            }
+
+            for (uint j = 0u; j < M + 1; ++j)
+            {
+                v[0, j] = mu1(Y(j));
+                v[N, j] = mu2(Y(j));
+            }
+
+            double current_accuracy;
+            double current = 0.0;
+            accuracy = 0.0;
+            counter = 0u;
+
+            do
+            {
+                accuracy = 0.0;
+
+                for (uint j = 1u; j < M; ++j)
+                {
+                    for (uint i = 1u; i < N; ++i)
+                    {
+                        current = v[i, j];
+
+                        v[i, j] = 0.0;
+                        v[i, j] += -k2 * (V(i, j + 1) + V(i, j - 1));
+                        v[i, j] += -h2 * (V(i + 1, j) + V(i - 1, j));
+                        v[i, j] += Function(i, j);
+                        v[i, j] /= a2;
+
+                        current_accuracy = Math.Abs(current - v[i, j]);
+
+                        if (accuracy < current_accuracy)
+                        {
+                            accuracy = current_accuracy;
+                        }
+                    }
+                }
+
+                counter++;
+
+            } while ((Nmax > counter) && (accuracy >= acc_max));
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            N = (uint)Nbox.Value;
+            M = (uint)Mbox.Value;
+            Nmax = (uint)NmaxBox.Value;
+            acc_max = double.Parse(AccuracyBox.Text);
+
+            ZeidelMethod();
+
+            IterLabel.Text = counter.ToString();
+            AccMaxLabel.Text = accuracy.ToString();
+
+            double[,] vStep = new double[N + 1, M + 1];
+
+            for (int j = 0; j < M + 1; ++j)
+            {
+                if (Table.Columns.Count <= j)
+                {
+                    Table.Columns.Add("", "");
+                }
+
+                for (int i = 0; i < N + 1; ++i)
+                {
+                    if (Table.Rows.Count <= i)
+                    {
+                        Table.Rows.Add();
+                    }
+
+                    vStep[i, j] = v[i, j];
+                }
+            }
+
+            for (int i = 0; i < N + 1; ++i)
+            {
+                for (int j = 0; j < M + 1; ++j)
+                {
+                    Table.Rows[i].Cells[j].Value = v[i, j];
+                }
+            }
+
+            N *= 2;
+            M *= 2;
+
+            ZeidelMethod();
+
+            double max_dif = 0;
+            int max_i = 0;
+            int max_j = 0;
+
+            for (int i = 0; i < (N - 1) / 2 + 1; ++i)
+            {
+                for(int j = 0; j < (M - 1) / 2 + 1; ++j)
+                {
+                    double cur_dif = Math.Abs(vStep[i, j] - v[i * 2, j * 2]);
+                    if(cur_dif > max_dif)
+                    {
+                        max_dif = cur_dif;
+                        max_i = i;
+                        max_j = j;
+                    }
+                }
+            }
+
+            MaxDifLabel.Text = max_dif.ToString();
+            DotLabel.Text = "В точке i = " + max_i.ToString() + ", j = " + max_j.ToString();
+        }
+
+
+        private double mu1(double y)
+        {
+            return Math.Exp(1.0 - Xo * Xo - y * y);
+        }
+
+        private double mu2(double y)
+        {
+            return Math.Exp(1.0 - Xn * Xn - y * y);
+        }
+
+        private double mu3(double x)
+        {
+            return Math.Exp(1.0 - x * x - Yo * Yo);
+        }
+
+        private double mu4(double x)
+        {
+            return Math.Exp(1.0 - x * x - Yn * Yn);
+        }
+
+        private double X(uint i)
+        {
+            return Xo + i * h;
+        }
+
+        private double Y(uint j)
+        {
+            return Yo + j * k;
+        }
+
+        private double V(uint i, uint j)
+        {
+            if (0u == i)
+            {
+                return mu1(Y(j));
+            }
+            if (N == i)
+            {
+                return mu2(Y(j));
+            }
+            if (0u == j)
+            {
+                return mu3(X(i));
+            }
+            if (M == j)
+            {
+                return mu4(X(i));
+            }
+
+            return v[i, j];
+        }
+
+        private double Function(uint i, uint j)
+        {
+            return 14.0 / 9.0 * Math.Exp(7.0 / 9.0);
+        }
+    }
+}
